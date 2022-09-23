c5989fcf1f9871777a4b81cacbe920a9b02c8c1a
Melissa Fields <melissamfields12@gmail.com>
2022-09-22 18:24:04 -0700

    commit tree f8ea4e34c942effc0fadfb5c2db83f921ea46c51

---
+++ index.js
@@ -0,0 +1,6 @@
+const divide = (parameter1, parameter2) => parameter1 / parameter2; 
+divide(2000, 100);
+const square = (value) => value * value;
+square(value);
+let add = (a, b) => a + b;
+add(a,b);
